e3c14b483f172fab957650158ce8636633ddeb2c
Jerry Min <jerryjmin7@gmail.com>
2023-01-19 23:06:57 +0000

    made it work it is just too slow

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from time import perf_counter
+
 def search(puzzle):
     for y, line in enumerate(puzzle):
         for x, num in enumerate(line):
@@ -9,10 +11,14 @@ def remove_zero(alist):
     return [x for x in alist if x != 0]
 
 
-def valid(y, x, puzzle, section):
+def valid(y, x, puzzle, section): #takes around 0.02 ms
+    a = perf_counter()
+    time = 0
     value = puzzle[y][x]
     line, column = remove_zero(puzzle[y]), remove_zero([line[x] for line in puzzle])
-    return True if len(line) == len(set(line)) and len(column) == len(set(column)) and value not in section else False
+    if len(line) == len(set(line)) and len(column) == len(set(column)) and value not in section:
+        return True, (perf_counter() - a)
+    return False, (perf_counter() - a)
 
 
 def begin(puzzle):
@@ -34,48 +40,45 @@ def begin(puzzle):
     return mutable, sections
 
 
-def solve_single(puzzle, seen = None):
+def solve_single(puzzle, seen=None):
     print(puzzle)
-    # TODO make it error on multiple solutions and refactor code is too slow on 17 hint puzzles ?
-    # make able checker better using valid
-    # change sections make each item in possible have its own puzzle (board)
-    #also got to ensure no repeats (set of tuple)
-
+    # TODO improve speed by making a row and column sections list (like with sections)
     if seen == None:
         seen = set()
     mutable, sections = begin(puzzle)
-    print(mutable)
     count = 0
     success = False
-    mutable_sections = [[] for x in range(9)]
-    #possible = [0]  #stores the index of mutable 
     index = 0
     multiple_solutions = False
     seen = set()
-    while True: #needs to change
+    while True:
+        count += 1
         y, x = mutable[index]
-        print(index, y, x)
         sections_index = x//3 + (y//3)*3
         success = False
-        new = puzzle[y][x] == 0 
-        print(puzzle[y][x], "should be 0")
+        if puzzle[y][x] == 0:
+            new = True
+        else:
+            sections[sections_index].pop()
         while success == False and puzzle[y][x] < 9:
             puzzle[y][x] += 1
-            success = valid(y, x, puzzle, sections[sections_index]) 
-        print(success, puzzle[y][x])
+            success, add_to_time = valid(y, x, puzzle, sections[sections_index])
+            time += add_to_time
         if success:
             sections[sections_index].append(puzzle[y][x])
             index += 1
             if index == len(mutable):
+                print(f"{time*1000} time ")
                 return puzzle
         else:
             index -= 1
-            if index < 0: #is negative indexing
+            if index < 0:
                 break
             if not new:
                 sections[sections_index].pop()
             puzzle[y][x] = 0
-    return puzzle 
+    print(count, "count")
+    return "FAIL"
 
 
 def sudoku_solver(puzzle):
@@ -83,13 +86,6 @@ def sudoku_solver(puzzle):
 
 
 if __name__ == "__main__":
-    print(solve_single(
-                [[0, 9, 6, 5, 0, 4, 0, 7, 1],
-                 [0, 2, 0, 1, 0, 0, 0, 0, 0],
-                 [0, 1, 4, 0, 9, 0, 6, 2, 3],
-                 [0, 0, 3, 0, 6, 0, 0, 8, 0],
-                 [0, 0, 8, 0, 5, 0, 4, 0, 0],
-                 [9, 0, 0, 4, 0, 0, 0, 0, 5],
-                 [7, 0, 0, 0, 0, 9, 0, 0, 0],
-                 [0, 0, 1, 0, 7, 5, 3, 4, 9],
-                 [2, 3, 0, 0, 4, 8, 1, 0, 7]]))
+    start = perf_counter()
+    print(solve_single([[8, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 3, 6, 0, 0, 0, 0, 0], [0, 7, 0, 0, 9, 0, 2, 0, 0], [0, 5, 0, 0, 0, 7, 0, 0, 0], [0, 0, 0, 0, 4, 5, 7, 0, 0], [0, 0, 0, 1, 0, 0, 0, 3, 0], [0, 0, 1, 0, 0, 0, 0, 6, 8], [0, 0, 8, 5, 0, 0, 0, 1, 0], [0, 9, 0, 0, 0, 0, 4, 0, 0]]))
+    print(f"{(perf_counter() - start) * 1000}")
